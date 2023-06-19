bbe990c6f3d6c4f9185de2d6d46df8502eecd530
AmandaKly <amandaklly0@gmail.com>
2023-06-19 10:43:22 -0300

    Criação de controllers

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Escola.Controllers
+{
+    [Route("[controller]")]
+    public class AlunosController : Controller
+    {
+        private readonly ILogger<AlunosController> _logger;
+
+        public AlunosController(ILogger<AlunosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
